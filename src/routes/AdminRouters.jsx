35fffc382d02072274e8bd46ca9051e3d2de123d
mafiaInternet <104735640+mafiaInternet@users.noreply.github.com>
2024-08-03 00:34:08 +0700

    Update AdminRouters.jsx

--- src/routes/AdminRouters.jsx
+++ src/routes/AdminRouters.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
 import Admin from '../admin/Admin';
 
 const AdminRouters = () => {
+    const location = useLocation()
     return (
         <div>
                 
                 <Routes>
                     <Route path='/*' element={<Admin></Admin>}></Route>
                 </Routes>
-        
+          {location.pathname == "/admin" && <Admin></Admin>}
         </div>
     );
 }
