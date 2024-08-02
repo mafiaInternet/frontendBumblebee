09313f84752747c8dbac66f579a2365fb6a8623d
mafiaInternet <104735640+mafiaInternet@users.noreply.github.com>
2024-08-02 23:51:53 +0700

    Update App.js 1

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const App = () => {
  
     
   <Routes>
-   <Route path="/*" element={<CustomerRouters></CustomerRouters>}></Route>
-   <Route path="/admin/*" element={<Admin></Admin>}></Route>
+   // <Route path="/*" element={<CustomerRouters></CustomerRouters>}></Route>
+   <Route path="/*" element={<Admin></Admin>}></Route>
    
   </Routes>
  
